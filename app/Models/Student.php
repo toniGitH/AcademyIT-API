a95e957cdeb20892b78f13b3abea0948312772bd
tonigith <91883442+toniGitH@users.noreply.github.com>
2025-03-17 16:06:18 +0100

    Created models: students, subjects, grades

---
+++ app/Models/Student.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name_1',
+        'last_name_2',
+        'age'
+    ];
+
+    public function grades()
+    {
+        return $this->hasMany(Grade::class);
+    }
+
+    public function subjects()
+    {
+        return $this->belongsToMany(Subject::class, 'grades')
+            ->withPivot('grade')
+            ->withTimestamps();
+    }
+}
